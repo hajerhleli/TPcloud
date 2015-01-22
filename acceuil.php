779e49a2dd21b6e9211a126786540aa7a6a2bf4b
hajerhleli <hajerhleli@users.noreply.github.com>
2015-01-22 18:42:42 +0100

    Create acceuil.php

---
+++ acceuil.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php include_once 'layout/header.php'; ?>
+<body id="page1">
+<!-- START PAGE SOURCE -->
+<div id="main">
+    <?php include_once 'layout/menu.php' ?>
+    <div id="content">
+        <ul class="box-list">
+            <li>
+                <div class="box">
+                    <div class="border-bot">
+                        <div class="right-bot-corner">
+                            <div class="left-bot-corner">
+                                <div class="inner">
+                                    <div class="box1">
+                                        <div class="inner">
+                                            <h4><b>Offre</b> Mobile</h4>
+                                            <p style="height: 85px;">Soyez Mobile et communiquez en toute liberté. Postpayé ou prépayé, profitez pleinement de nos offres, nos promos, nos services et nos packs. <a href="offre-mobile.php"><img src="images/arrow.gif" alt="" /></a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="alt">
+                <div class="box">
+                    <div class="border-bot">
+                        <div class="right-bot-corner">
+                            <div class="left-bot-corner">
+                                <div class="inner">
+                                    <div class="box1">
+                                        <div class="inner">
+                                            <h4><b>Offre</b> Fixe</h4>
+                                            <p style="height: 85px;">Découvrez nos offres illimitées fixes et bénéficiez des tarifs de communication les moins chers. <a href="offre-fixe.php"><img src="images/arrow.gif" alt="" /></a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="last">
+                <div class="box">
+                    <div class="border-bot">
+                        <div class="right-bot-corner">
+                            <div class="left-bot-corner">
+                                <div class="inner">
+                                    <div class="box1">
+                                        <div class="inner">
+                                            <h4><b>Offre</b> Internet</h4>
+                                            <p style="height: 85px;">Avec l’ADSL de Tunisie Telecom, surfez sur internet et bénéficiez du confort du haut débit. <a href="offre-internet.php"><img src="images/arrow.gif" alt="" /></a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        <div class="indent">
+            <div class="wrapper">
+                <div class="col-1">
+                    <h3><b>Bienvenu à</b> Tunisie Telecom Zaghouan</h3>
+                    <p>Eusus consequam vitae habitur amet nullam vitae condis phasellus sed justo. Orcivel mollis intesque eu sempor ridictum a non laorem lacingilla wisi. Nuncrhoncus eros maurien ulla facilis.</p>
+                    <div class="img-box1"><img src="images/ttshop.jpg" alt="ttshop.jpg" style="height: 125px;" />
+                        <p class="p0">Eusus consequam vitae habitur amet nullam vitae condis phasellus sed justo. Orcivel mollis intesque eu <a href="#">sempor ridictum</a> a non laorem lacingilla wisi. Nuncrhoncus eros maurien ulla facilis tor mauris tincidunt et vitae morbi. Velelit condimentes in laorem quis nullamcorper nam fauctor feugiat pellent.</p>
+                    </div>
+                    <p>Eusus consequam vitae habitur amet nullam vitae condis phasellus sed justo. Orcivel mollis intesque eu sempor <a href="#">ridictum a non</a> laorem lacingilla wisi.</p>
+                    <p class="p0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+                <?php include_once 'layout/news.php' ?>
+            </div>
+        </div>
+    </div>
+    <?php include_once 'layout/footer.php' ?>
+</div>
+
+<!-- END PAGE SOURCE -->
+</html>
