fc997ecac64b5700a5e2e0bb53691c6b6f0553f1
hajerhleli <hajerhleli@users.noreply.github.com>
2015-01-22 18:36:35 +0100

    Create sitemap.php

---
+++ sitemap.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php include_once 'layout/header.php'; ?>
+<body id="page6">
+<!-- START PAGE SOURCE -->
+<div id="main">
+    <?php include_once 'layout/menu.php' ?>
+  <div id="content">
+    <div class="box">
+      <div class="border-bot">
+        <div class="right-bot-corner">
+          <div class="left-bot-corner">
+            <div class="inner">
+              <div class="box1 alt">
+                <div class="inner">
+                  <h4><b>Site</b> Map</h4>
+                  <p class="p0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="indent">
+      <div class="wrapper">
+        <div class="col-1">
+          <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.</p>
+          <ul class="sitemap">
+            <li><a href="accueil.php">Accueil</a>
+                <ul>
+                    <li>Offre Mobile</li>
+                    <li>Offre Fixe</li>
+                    <li>Offre Internet</li>
+                </ul>
+            </li>
+            <li><a href="services.php">Nouveauté</a></li>
+            <li><a href="about-us.php">Qui Somme Nous</a></li>
+
+            <li><a href="contact-us.php">Contact</a></li>
+            <li><a href="sitemap.php">Site Map</a></li>
+          </ul>
+          <p class="p0">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque.</p>
+        </div>
+          <?php include_once 'layout/news.php' ?>
+      </div>
+    </div>
+  </div>
+    <?php include_once 'layout/footer.php' ?>
+</div>
+
+<!-- END PAGE SOURCE -->
