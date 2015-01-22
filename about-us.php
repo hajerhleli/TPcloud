89ebbf078473199482bb0a48f383582a9bb421cb
hajerhleli <hajerhleli@users.noreply.github.com>
2015-01-22 18:22:36 +0100

    Create about-us.php

---
+++ about-us.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php include_once 'layout/header.php'; ?>
+<body id="page2">
+<!-- START PAGE SOURCE -->
+<div id="main">
+    <?php include_once 'layout/menu.php' ?>
+  <div id="content">
+    <div class="box">
+      <div class="border-bot">
+        <div class="right-bot-corner">
+          <div class="left-bot-corner">
+            <div class="inner">
+              <div class="box1 alt">
+                <div class="inner">
+                  <h4><b>Qui</b> Somme Nous ?</h4>
+                  <p class="p0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="indent">
+      <div class="wrapper">
+        <div class="col-1">
+          <h3><b>Notre</b> équipe</h3>
+          <ul class="list">
+            <li><img src="images/2page-img1.jpg" alt="" /><a href="#">Team Member One</a><br />
+              Eusus consequam vitae habitur amet nullam vitae condis phasellus sed justo. Orcivel mollis intesque eu sempor ridictum a non laorem lacingilla wisi. Nuncrhoncus eros maurien ulla facilis tor.</li>
+            <li><img src="images/2page-img2.jpg" alt="" /><a href="#">Another Team Member</a><br />
+              Eusus consequam vitae habitur amet nullam vitae condis phasellus sed justo. Orcivel mollis intesque eu sempor ridictum a non laorem lacingilla wisi. Nuncrhoncus eros maurien ulla facilis tor.</li>
+            <li><img src="images/2page-img3.jpg" alt="" /><a href="#">Another Team Member</a><br />
+              Eusus consequam vitae habitur amet nullam vitae condis phasellus sed justo. Orcivel mollis intesque eu sempor ridictum a non laorem lacingilla wisi. Nuncrhoncus eros maurien ulla facilis tor.</li>
+            <li><img src="images/2page-img4.jpg" alt="" /><a href="#">Another Team Member</a><br />
+              Eusus consequam vitae habitur amet nullam vitae condis phasellus sed justo. Orcivel mollis intesque eu sempor ridictum a non laorem lacingilla wisi. Nuncrhoncus eros maurien ulla facilis tor.</li>
+          </ul>
+        </div>
+          <?php include_once 'layout/news.php' ?>
+      </div>
+    </div>
+  </div>
+    <?php include_once 'layout/footer.php' ?>
+</div>
+
+<!-- END PAGE SOURCE -->
